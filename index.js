a412d9a44790c955f5f81b29971a166e4f6f611b
Kevin Lacson <lacsonky@gmail.com>
2021-12-28 14:47:41 -0500

    added questions.js for question array

--- index.js
+++ index.js
@@ -1,81 +1,23 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const questions = require("./utils/questions");
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [
-  //   {
-  //     type: 'input',
-  //     message: 'Title:',
-  //     name: 'title',
-  //   },
-  //   {
-  //     type: 'input',
-  //     message: 'Description:',
-  //     name: 'description',
-  //   },
-  //   {
-  //     type: 'input',
-  //     message: 'Author:',
-  //     name: 'author',
-  //   },
-  //   {
-  //     type: 'input',
-  //     message: 'GitHub profile:',
-  //     name: 'github',
-  //   },
-  //   {
-  //     type: 'input',
-  //     message: 'email:',
-  //     name: 'email',
-  //   },
-  //   {
-  //     type: 'input',
-  //     message: 'Installation instructions:',
-  //     name: 'install',
-  //   },
-  //   {
-  //     type: 'input',
-  //     message: 'Usage:',
-  //     name: 'usage',
-  //   },
-  //   {
-  //     type: 'input',
-  //     message: 'Contribution:',
-  //     name: 'contribute',
-  //   },
-  //   {
-  //     type: 'input',
-  //     message: 'Test:',
-  //     name: 'test',
-  //   },
-  {
-    type: 'list',
-    message: 'License:',
-    choices: [
-      'MIT',
-      new inquirer.Separator(),
-      'GNU GPLv3',
-      new inquirer.Separator(),
-      'ISC',
-    ],
-    name: 'license',
-  },
-];
 
 // TODO: Create a function to write README file
 function writeToFile(data) {
-  fs.writeFile('README.md', data, err =>
-    err ? console.log(err) : console.log('Writing README...')
-  );
+    fs.writeFile("README.md", data, err =>
+        err ? console.log(err) : console.log("Writing README...")
+    );
 }
 
 // TODO: Create a function to initialize app
 function init() {
-  inquirer
-    .prompt(questions)
-    .then(data => generateMarkdown(data))
-    .then(data => writeToFile(data));
+    inquirer
+        .prompt(questions)
+        .then(data => generateMarkdown(data))
+        .then(data => writeToFile(data));
 }
 
 // Function call to initialize app
---
+++ utils/questions.js
@@ -0,0 +1,63 @@
+const inquirer = require("inquirer");
+
+const questions = [
+    //   {
+    //     type: 'input',
+    //     message: 'Title:',
+    //     name: 'title',
+    //   },
+    //   {
+    //     type: 'input',
+    //     message: 'Description:',
+    //     name: 'description',
+    //   },
+    //   {
+    //     type: 'input',
+    //     message: 'Author:',
+    //     name: 'author',
+    //   },
+    //   {
+    //     type: 'input',
+    //     message: 'GitHub profile:',
+    //     name: 'github',
+    //   },
+    //   {
+    //     type: 'input',
+    //     message: 'email:',
+    //     name: 'email',
+    //   },
+    //   {
+    //     type: 'input',
+    //     message: 'Installation instructions:',
+    //     name: 'install',
+    //   },
+    //   {
+    //     type: 'input',
+    //     message: 'Usage:',
+    //     name: 'usage',
+    //   },
+    //   {
+    //     type: 'input',
+    //     message: 'Contribution:',
+    //     name: 'contribute',
+    //   },
+    //   {
+    //     type: 'input',
+    //     message: 'Test:',
+    //     name: 'test',
+    //   },
+    {
+        type: "list",
+        message: "License:",
+        choices: [
+            "MIT",
+            new inquirer.Separator(),
+            "GNU GPLv3",
+            new inquirer.Separator(),
+            "ISC",
+        ],
+        name: "license",
+    },
+];
+
+module.exports = questions;
